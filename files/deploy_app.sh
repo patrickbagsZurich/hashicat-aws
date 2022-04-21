d7bc7a1c1e8309b6e4e99f4d6a07fc2066872ca2
patrickbagsZurich <99294592+patrickbagsZurich@users.noreply.github.com>
2022-04-21 13:29:04 -0500

    thoughtful text

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to My app. Here's some thoughtful text.
+  Welcome to My app. Here's some thoughtful text after I did this wrong the first time.
   <!-- END -->
 
   </div>
